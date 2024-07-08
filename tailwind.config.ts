b87d7155f4af51208f21f2f2b6e7e07aa0ccb735
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2024-07-07 23:20:41 -0500

    Feat: add missing border radius size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,7 +55,7 @@ const config = {
       },
       borderRadius: {
         lg: 'var(--radius)',
-
+        md: `calc(var(--radius) - 2px)`,
         sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: {
