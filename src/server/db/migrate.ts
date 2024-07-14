fe8064bdb84d8cba8db88ba79068d72632cd5d8e
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2024-07-14 16:26:30 -0500

    Feat: add initial configuration for next auth

---
+++ src/server/db/migrate.ts
@@ -0,0 +1,28 @@
+import { sql } from '@vercel/postgres';
+
+import { drizzle } from 'drizzle-orm/vercel-postgres';
+
+import { migrate } from 'drizzle-orm/vercel-postgres/migrator';
+
+const databaseUrl = process.env.POSTGRES_URL;
+
+if (!databaseUrl) {
+  throw new Error('No connection string provided');
+}
+
+export const db = drizzle(sql);
+
+const main = async () => {
+  try {
+    await migrate(db, {
+      migrationsFolder: 'src/server/db/migrations',
+    });
+
+    console.log('Migration successful');
+  } catch (err) {
+    console.error('Migration failed', err);
+    process.exit(1);
+  }
+};
+
+main();
