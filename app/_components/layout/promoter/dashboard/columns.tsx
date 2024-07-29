9d05a7609a5223a21e09ee74171caa30a41142db
gianellacoronel <gia.ne12@hotmail.com>
2024-07-29 17:12:50 -0500

    feat(event form table): add menu Edit/Delete

--- app/_components/layout/promoter/dashboard/columns.tsx
+++ app/_components/layout/promoter/dashboard/columns.tsx
@@ -1,63 +1,100 @@
-'use client'
+'use client';
 
-import { ColumnDef } from "@tanstack/react-table"
+import { Button } from '@/app/_components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/app/_components/ui/dropdown-menu';
+import { ColumnDef } from '@tanstack/react-table';
+import { MoreHorizontal } from 'lucide-react';
 
 export type Event = {
-    id: string
-    name: string
-    startDate: Date
-    endDate: Date,
-    category: string
-    //status: "confirmed" | "cancelled"
-}
+  id: string;
+  name: string;
+  startDate: Date;
+  endDate: Date;
+  category: string;
+  //status: "confirmed" | "cancelled"
+};
 
 export const columns: ColumnDef<Event>[] = [
-    {
-        accessorKey: "name",
-        header: "Name",
-    },
-    {
-        accessorKey: "category",
-        header: "Category",
-        meta: 'select',
-    },
-    {
-        accessorKey: "startDate",
-        header: "Start Date",
-        cell: ({ row }) => {
-            const date = new Date(row.getValue("startDate"));
-            const formatted = new Intl.DateTimeFormat("en-US", {
-                year: 'numeric',
-                month: 'short',
-                day: 'numeric',
-                hour: '2-digit',
-                minute: '2-digit',
-                hour12: true // Set to false if you prefer 24-hour format
-            }).format(date);
+  {
+    accessorKey: 'name',
+    header: 'Name',
+  },
+  {
+    accessorKey: 'category',
+    header: 'Category',
+    meta: 'select',
+  },
+  {
+    accessorKey: 'startDate',
+    header: 'Start Date',
+    cell: ({ row }) => {
+      const date = new Date(row.getValue('startDate'));
+      const formatted = new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true, // Set to false if you prefer 24-hour format
+      }).format(date);
 
-            return <div>{formatted}</div>
-        },
+      return <div>{formatted}</div>;
     },
-    {
-        accessorKey: "endDate",
-        header: "End Date",
-        cell: ({ row }) => {
-            const date = new Date(row.getValue("endDate"));
-            const formatted = new Intl.DateTimeFormat("en-US", {
-                year: 'numeric',
-                month: 'short',
-                day: 'numeric',
-                hour: '2-digit',
-                minute: '2-digit',
-                hour12: true // Set to false if you prefer 24-hour format
-            }).format(date);
+  },
+  {
+    accessorKey: 'endDate',
+    header: 'End Date',
+    cell: ({ row }) => {
+      const date = new Date(row.getValue('endDate'));
+      const formatted = new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true, // Set to false if you prefer 24-hour format
+      }).format(date);
 
-            return <div>{formatted}</div>
-        },
+      return <div>{formatted}</div>;
     },
-    // {
-    //     accessorKey: "status",
-    //     header: "Status",
-    // },
-]
+  },
 
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="text-destructive">
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  }, // {
+  //     accessorKey: "status",
+  //     header: "Status",
+  // },
+];
