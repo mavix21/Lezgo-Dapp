702d60e55cbdf0787485261556cd4002366bf737
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2024-08-01 13:02:28 -0500

    Fix: vertical padding

--- app/(services)/promoter/dashboard/page.tsx
+++ app/(services)/promoter/dashboard/page.tsx
@@ -38,7 +38,7 @@ export default async function Home() {
   const data = await getEvents();
   const categories = await getEventCategories();
   return (
-    <main className="bg-background text-foreground flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+    <main className="bg-background text-foreground flex-1 items-start gap-4 p-4 sm:px-6 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
         <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
           <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
