147d92838166dff88b992f74c4b3126e440bbe14
gianella <gianella.coronel@inventiva-solutions.com>
2024-08-01 16:44:34 -0500

    fix(design dashboard): fix navbar scroll

--- app/(services)/promoter/layout.tsx
+++ app/(services)/promoter/layout.tsx
@@ -16,12 +16,12 @@ export default function PromoterLayout({
 }) {
   return (
     <div className="antialiased">
-      <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="grid min-h-svh w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
         <div className="hidden border-r bg-muted/40 md:block">
           <Sidebar />
         </div>
         <div className="flex flex-col">
-          <header className="flex h-14 items-center gap-4 border-b justify-between bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+          <header className="flex h-14 items-center gap-4 border-b justify-between bg-muted/40 px-4 lg:px-6">
             <SidebarMobile />
             <UserSettingsDropdown />
           </header>
--- app/_components/layout/promoter/Sidebar.tsx
+++ app/_components/layout/promoter/Sidebar.tsx
@@ -35,7 +35,7 @@ const menuItems = [
 export const Sidebar = () => {
   return (
     <div id="menu" className="flex h-full max-h-screen flex-col gap-2">
-      <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+      <div className="flex h-14 items-center border-b px-4 lg:px-6">
         <Link href="/public" className="flex items-center gap-2 font-semibold">
           <Package2 className="h-6 w-6" />
           <span>Acme Inc</span>
